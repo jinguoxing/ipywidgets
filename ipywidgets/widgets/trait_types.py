82472135d04678ed0bb63ab017adf0b9e5411260
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-09-21 11:29:41 +0200

    Ensure Color.validate fails with TraitError
    If value was not string, it would previously fail with `AttributeError`, making `Union` with a color fail (its validator catches TraitErrors).

--- ipywidgets/widgets/trait_types.py
+++ ipywidgets/widgets/trait_types.py
@@ -5,9 +5,12 @@
 Trait types for html widgets.
 """
 
+import six
 import re
 import traitlets
 import datetime as dt
+
+from .util import text_type
 
 
 _color_names = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred ', 'indigo ', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'transparent', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen']
@@ -23,8 +26,9 @@ class Color(traitlets.Unicode):
     def validate(self, obj, value):
         if value is None and self.allow_none:
             return value
-        if value.lower() in _color_names or _color_re.match(value):
-            return value
+        if isinstance(value, text_type):
+            if value.lower() in _color_names or _color_re.match(value):
+                return value
         self.error(obj, value)
         
 
@@ -48,7 +52,7 @@ class FullColor(Color):
     def validate(self, obj, value):
         try:
             return super(FullColor, self).validate(obj, value)
-        except TraitError:
+        except traitlets.TraitError:
             if _color_hexa_re.match(value) or _color_rgbhsl_re.match(value):
                 return value
             raise
