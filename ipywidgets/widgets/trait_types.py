458b83e630f681ad859622f397d62b3a2e9472ef
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-09-21 10:46:56 +0200

    Respect allow_none for Color, add FullColor
    - Respects `allow_none` for `Color` trait
    - Adds trait type `FullColor` that uses the expanded HTML color definitions.

--- ipywidgets/widgets/trait_types.py
+++ ipywidgets/widgets/trait_types.py
@@ -21,9 +21,39 @@ class Color(traitlets.Unicode):
     default_value = traitlets.Undefined
 
     def validate(self, obj, value):
+        if value is None and self.allow_none:
+            return value
         if value.lower() in _color_names or _color_re.match(value):
             return value
         self.error(obj, value)
+        
+
+_color_hexa_re = re.compile(r'^#[a-fA-F0-9]{4}(?:[a-fA-F0-9]{4})?$')
+
+_color_frac_percent = r'\s*(\d+(\.\d*)?|\.\d+)?%?\s*'
+_color_int_percent = r'\s*\d+%?\s*'
+
+_color_rgb = r'rgb\({ip},{ip},{ip}\)'
+_color_rgba = r'rgba\({ip},{ip},{ip},{fp}\)'
+_color_hsl = r'hsl\({fp},{fp},{fp}\)'
+_color_hsla = r'hsla\({fp},{fp},{fp},{fp}\)'
+
+_color_rgbhsl_re = re.compile('({0})|({1})|({2})|({3})'.format(
+    _color_rgb, _color_rgba, _color_hsl, _color_hsla
+).format(ip=_color_int_percent, fp=_color_frac_percent))
+
+
+class FullColor(Color):
+
+    def validate(self, obj, value):
+        try:
+            return super(FullColor, self).validate(obj, value)
+        except TraitError:
+            if _color_hexa_re.match(value) or _color_rgbhsl_re.match(value):
+                return value
+            raise
+        self.error(obj, value)
+
 
 
 class Datetime(traitlets.TraitType):
