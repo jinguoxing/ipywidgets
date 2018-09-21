b0ed63e295309dee3526a07cc9c692cc23931693
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-09-21 11:24:56 +0200

    Extract text_type for reuse

---
+++ ipywidgets/widgets/util.py
@@ -0,0 +1,9 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+# six is not a direct dependency of this module
+# This replicates six.text_type
+try:
+    text_type = unicode
+except NameError:
+    text_type = str
